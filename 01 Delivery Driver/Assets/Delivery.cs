e9fcc416ad1af92b7dd5d53828316d0d5f0515e4
Mihai Man <mitaman89@yahoo.com>
2022-07-19 11:21:45 +0300

    Change car color on pickup

--- 01 Delivery Driver/Assets/Delivery.cs
+++ 01 Delivery Driver/Assets/Delivery.cs
@@ -4,26 +4,39 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    bool hasPackage = false;
     [SerializeField] float pickupDelay = 0.5f;
+    bool hasPackage = false;
 
-    private void OnCollisionEnter2D(Collision2D other) {
+    Color32 defaultColor = new Color32(255, 255, 255, 255);
+    SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
         Debug.Log("Bump!");
     }
 
-    private void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other)
+    {
         switch (other.tag) {
             case "Package":
                 if (!hasPackage) {
                     hasPackage = true;
                     Debug.Log("Package picked up");
+                    Color32 otherColor = other.GetComponent<SpriteRenderer>().color;
                     Destroy(other.gameObject, pickupDelay);
+                    spriteRenderer.color = otherColor;
                 }
                 break;
             case "Customer":
                 if(hasPackage) {
                     hasPackage = false;
                     Debug.Log("Package delivered");
+                    spriteRenderer.color = defaultColor;
                 }
                 break;
             default:
