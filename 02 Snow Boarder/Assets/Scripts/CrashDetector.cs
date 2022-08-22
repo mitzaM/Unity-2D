0d2963e2ce806e77097a1e80a8f237557f3359c4
Mihai Man <mitaman89@yahoo.com>
2022-08-22 13:11:35 +0300

    Reload scene on triggers

--- 02 Snow Boarder/Assets/Scripts/CrashDetector.cs
+++ 02 Snow Boarder/Assets/Scripts/CrashDetector.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Ground") {
-            Debug.Log("Boop!");
+            SceneManager.LoadScene("Level1");
         }
     }
 }
--- 02 Snow Boarder/Assets/Scripts/FinishLine.cs
+++ 02 Snow Boarder/Assets/Scripts/FinishLine.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.tag == "Player") {
-            Debug.Log("FINISHED!");
+            SceneManager.LoadScene("Level1");
         }
     }
 }
